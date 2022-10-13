3cf99b2cf1e19cc1dd256464d8fa306bc73ba857
Ayush Jain <ajsa02812@gmail.com>
2022-10-13 18:29:27 +0530

    hello

---
+++ first.vue
@@ -0,0 +1,21 @@
+<template><div> Hello {{ name }}</div>
+    
+</template>
+<script>
+    export default{
+        name: 'first',
+        data(){
+            return{
+                greet: "hello",
+                name: 'Ayush Jain',
+            };
+        },
+    };
+</script>
+<style>
+   #name{
+       color: red;
+       text-align: center;
+       margin-top: 60px;
+   }
+</style>
